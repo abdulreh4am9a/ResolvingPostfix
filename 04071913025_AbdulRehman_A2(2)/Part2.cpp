b6ec8163bb933b5c182260891e45add8d0097a84
abdulreh4am9a <abdulreh4am9a@gmail.com>
2020-11-10 21:13:13 +0500

    Modified

--- 04071913025_AbdulRehman_A2(2)/Part2.cpp
+++ 04071913025_AbdulRehman_A2(2)/Part2.cpp
@@ -3,53 +3,59 @@
 using namespace std;
 template<typename T>
 struct node {
-    T data;
-    node<T>* next;
+	T data;
+	node<T>* next;
 };
 template<typename T>
 class stack {
-    node<T>* top_ptr;
+	node<T>* top_ptr;
 public:
-    stack() {
-        top_ptr = NULL;
-    }
-    ~stack() {
-        make_empty();
-    }
-    void push(const T& val) {
-        node<T>* temp;
-        temp = new node<T>;
-        temp->data = val;
-        temp->next = top_ptr;
-        top_ptr = temp;
-    }
-    void pop() {
-        node<T>* temp;
-        temp = top_ptr;
-        top_ptr = top_ptr->next;
-        delete temp;
-    }
-    T top() const {
-        return top_ptr->data;
-    }
-    bool empty() const {
-        return top_ptr == NULL;
-    }
-    bool full() const {
-        node<T>* temp;
-        temp = new node<T>;
-        if (temp == NULL)
-            return true;
-        else {
-            delete temp;
-            return false;
-        }
-    }
-    void make_empty() {
-        while (!empty()) {
-            pop();
-        }
-    }
+	stack() {
+		top_ptr = NULL;
+	}
+	~stack() {
+		make_empty();
+	}
+	void push(const T& val) {
+		if (full())
+			throw("Stack Overflow!");
+		node<T>* temp;
+		temp = new node<T>;
+		temp->data = val;
+		temp->next = top_ptr;
+		top_ptr = temp;
+	}
+	void pop() {
+		if (empty())
+			throw("Stack Underflow!");
+		node<T>* temp;
+		temp = top_ptr;
+		top_ptr = top_ptr->next;
+		delete temp;
+	}
+	T top() const {
+		if (empty())
+			throw("Stack is empty!");
+		return top_ptr->data;
+	}
+	bool empty() const {
+		return top_ptr == NULL;
+	}
+	bool full() const {
+		node<T>* temp;
+		temp = new node<T>;
+		if (temp == NULL)
+			return true;
+		else {
+			delete temp;
+			return false;
+		}
+	}
+	void make_empty() {
+		while (!empty()) {
+			pop();
+		}
+	}
 };
 bool is_operator(char x)
 {
