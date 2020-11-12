dd2a0a651b911dedfd49afb2a53eeff841111fcd
abdulreh4am9a <abdulreh4am9a@gmail.com>
2020-11-12 14:38:53 +0500

    Final

--- 04071913025_AbdulRehman_A2(2)/Part2.cpp
+++ 04071913025_AbdulRehman_A2(2)/Part2.cpp
@@ -1,17 +1,41 @@
 #include"stack.h"
 #include<string>
-bool is_operator(char x)
+int power(int base, int exp) {
+    int result = 1;
+    while (exp != 0) {
+        result *= base;
+        exp--;
+    }
+    return result;
+}
+bool is_operator(char x) //simple function to check whether the character is an operator or not
 {
     return (x == '+' || x == '-' || x == '*' || x == '/' || x == '^' || x == '%');
 }
-int postfixToResult(string postfix) {
-    int i = 0, n1, n2;
-    stack<int> s;
-    while (postfix[i] != '\0') {
-        if (isdigit(postfix[i])) {
-            s.push(postfix[i] - 48);
+int is_digit(char x) { //simple function to check whether the character is a digit and if it is digit returns its integer value else returns -1
+    switch (x)
+    {
+    case '0':return 0;break;
+    case '1':return 1;break;
+    case '2':return 2;break;
+    case '3':return 3;break;
+    case '4':return 4;break;
+    case '5':return 5;break;
+    case '6':return 6;break;
+    case '7':return 7;break;
+    case '8':return 8;break;
+    case '9':return 9;break;
+    default:return -1;break;
+    }
+}
+int postfixToResult(string postfix) { //function to evaluate the postfix notation
+    int i = 0, n1, n2; //i for loop and n1 n2 to get two operands from stack
+    stack<int> s; //stack to perform relevant operations
+    while (postfix[i] != '\0') { //reading the string index by index until the null operator arrives
+        if (is_digit(postfix[i]) != -1) { //using is_digit function to check whether a character is digit or not
+            s.push(is_digit(postfix[i])); //if character is digit pushing its integer value to stack
         }
-        else if (is_operator(postfix[i])) {
+        else if (is_operator(postfix[i])) { //else if the character is operator poping out two operands from stack and performing that operator on them and again pushing them to stack
             n1 = s.top();
             s.pop();
             n2 = s.top();
@@ -34,7 +58,7 @@ int postfixToResult(string postfix) {
                 s.push(n2 % n1);
                 break;
             case '^':
-                s.push(n2 ^ n1);
+                s.push(power(n2,n1));
                 break;
             default:
                 break;
@@ -42,7 +66,7 @@ int postfixToResult(string postfix) {
         }
         i++;
     }
-    return s.top();
+    return s.top(); //in the end the result is present in stack so returning it
 }
 
 int main(int argc, char* argv[])
